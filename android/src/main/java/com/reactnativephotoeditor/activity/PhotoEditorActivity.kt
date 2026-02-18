2576ab6067d4093a492c10226181fa5aaae695b3
Bellets96 <166222718+Bellets96@users.noreply.github.com>
2026-02-18 09:27:07 +0100

    Update PhotoEditorActivity.kt

--- android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
+++ android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
@@ -335,9 +335,9 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
   private fun showSaveDialog() {
     val builder = AlertDialog.Builder(this)
     builder.setMessage(getString(R.string.msg_save_image))
-    builder.setPositiveButton("Save") { _: DialogInterface?, _: Int -> saveImage() }
-    builder.setNegativeButton("Cancel") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
-    builder.setNeutralButton("Discard") { _: DialogInterface?, _: Int -> onCancel() }
+    builder.setPositiveButton("Salva") { _: DialogInterface?, _: Int -> saveImage() }
+    builder.setNegativeButton("Cancella") { dialog: DialogInterface, _: Int -> dialog.dismiss() }
+    builder.setNeutralButton("Scarta modifiche") { _: DialogInterface?, _: Int -> onCancel() }
     builder.create().show()
   }
 
